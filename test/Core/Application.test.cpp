ec6caf93e53bedda5bb73c807e547dc71ff7918a
Chris P Bacon <chris@kruining.eu>
2017-01-29 00:28:53 +0100

    added a test file for the Application class

---
+++ test/Core/Application.test.cpp
@@ -0,0 +1,8 @@
+#define BOOST_TEST_MODULE testApplication
+#include <boost/test/unit_test.hpp>
+#include "src/Core/Application.h"
+
+BOOST_AUTO_TEST_CASE(testApplication)
+{
+    BOOST_CHECK(1 == 1);
+}
